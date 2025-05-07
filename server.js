af84f99c40fbc482ec89014c4e475d0d287831a5
Vallyblink <115736687+Vallyblink@users.noreply.github.com>
2025-05-07 17:14:31 +0300

    Cors

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const recipeRouter = require('./routes/recipebook')
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin: '*',
+    methods: 'GET,POST', 
+  }));
 app.use('/api/recipes', recipeRouter);
 
 app.listen( PORT, () => {
